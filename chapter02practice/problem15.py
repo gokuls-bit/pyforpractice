27c8da119be3a00abec6b7bf72bc2a3e16f489e7
gokuls-bit <gokulsant684@gmail.com>
2025-05-23 00:57:42 +0530

    initial commit for pyforpractice

---
+++ chapter02practice/problem15.py
@@ -0,0 +1,28 @@
+#a = int(input("enter the number you want: " ))
+#b= int(input("enter the number you want: "))
+
+#sum = a+b
+#print (sum)
+#a=  int(input("enter the number you want: " ))
+#z = int(input("enter the number you want: "))
+ 
+
+#D =  a/z
+#print(D, a%z)
+
+#a = 34
+#b = 80
+
+#ans = (a >= b)
+#print (ans)
+ 
+#a = int(input("enter the number you want: " ))
+#b= a**2
+#print(b)
+
+#data = {50,100,80,20,50}
+#average = sum(data) /  len(data)
+#print (average)
+
+a = int(input("enter the number you want: " ))
+print(type(a))
