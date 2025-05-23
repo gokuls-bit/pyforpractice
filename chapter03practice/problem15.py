9679b83e42df333e5fa7fcbd2f0eb34f83aa5158
gokuls-bit <gokulsant684@gmail.com>
2025-05-23 18:35:20 +0530

    initial commit for pyforpractice

---
+++ chapter03practice/problem15.py
@@ -0,0 +1,26 @@
+'''import lorem
+01  name = input("enter your name ")
+print (f"goodmorning  {name}")
+
+02  letter = 
+Dear <|Name|>
+you are selected!
+<|Date|>''' '''
+
+e =input("enter your Name")
+f =input("enter Date")
+new_letter = letter.replace("<|Date|>",f)
+new_letter = new_letter.replace("<|Name|>",e)
+print (new_letter)
+
+03 text = "Gokul Kumar Sant is a passionate learner      who constantly seeks to improve his skills."
+checker= text.find("   ")
+print (checker)
+
+04 text = "Gokul Kumar Sant is a passionate learner      who constantly seeks to improve his skills."
+checker= text.replace("   "," ")
+print (checker)'''
+
+'05'
+letter = "Dear Harry,\n this python cource is nice . \nThanks!"
+print(letter)
---
+++ chapter04practice/problem15.py
@@ -0,0 +1,27 @@
+''' 01 fruits = []
+f1 = input("enterfruit 1 : ")
+fruits.append(f1)
+f2 = input("enterfruit 2 : ")
+fruits.append(f2)
+f3 = input("enterfruit 3 : ")
+fruits.append(f3)
+f4 = input("enterfruit 4 : ")
+fruits.append(f4)
+f5 = input("enterfruit 5 : ")
+fruits.append(f5)
+f6 = input("enterfruit 6 : ")
+fruits.append(f6)
+f7 = input("enterfruit 7 : ")
+fruits.append(f7)
+print(fruits)
+ 
+ 02 marks = [45,78,65,41,23,89]
+print(sorted(marks))
+ 
+ #03tuple cant be changed '
+04 numbers=[45,96,89,69]
+
+print(sum(numbers))'''
+
+a=(7,0,8,0,0,9)
+print(a.count(0))
