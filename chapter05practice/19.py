77246a4cd42ae99e52960d2531b1ef74055fb8e6
gokuls-bit <gokulsant684@gmail.com>
2025-05-24 19:09:37 +0530

    reinitializing

---
+++ chapter05practice/19.py
@@ -0,0 +1,54 @@
+dictionary={
+    "samay":"time",
+    "awaj" : "noise",
+    "kapda":"cloth",
+    "majak":"joke",
+    "mehak":"fregrence",
+    "uper":"up",
+    "niche":"down",
+}
+print(dictionary)
+
+#n1 = int(input("enter any no. : "))
+#n#11 = int(input("enter any no. : "))
+#n2 = int(input("enter any no. : "))
+#n4 = int(input("enter any no. : "))
+#n3 = int(input("enter any no. : "))
+#n5 = int(input("enter any no. : "))
+#n6 = int(input("enter any no. : "))
+#n7 = int(input("enter any no. : "))
+#n8 = int(input("enter any no. : "))
+#set_num = { n1,n11,n2,n3,n4,n5,n6,n7,n8}
+#print(set_num)
+
+p={18,"18"}
+print(p)
+
+#s = set()
+#s.add(20)
+#s.add(20.0)
+#s.add('20')
+#print(s)
+#s={}
+#type  = dictnary
+#s={}
+#name=input("enter friend name: ")
+#lang =input("enter friend lang : ")
+#s.update({name:lang})
+#name=input("enter friend name: ")
+#lang =input("enter friend lang : ")
+#s.update({name:lang})
+
+#name=input("enter friend name: ")
+#lang =input("enter friend lang : ")
+#s.update({name:lang})
+
+#name=input("enter friend name: ")
+#lang =input("enter friend lang : ")
+#s.update({name:lang})
+
+#print(s)
+ 
+ #values to be updated
+s={8,7,9,6,"harry",[1,2]}
+s[4][0]=9
