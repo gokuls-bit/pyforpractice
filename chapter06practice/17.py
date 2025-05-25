efe6e27b37b74e167186c5083a3f15a7ad8ccd1a
gokuls-bit <gokulsant684@gmail.com>
2025-05-26 00:29:10 +0530

    reinitializing

---
+++ chapter06practice/17.py
@@ -0,0 +1,102 @@
+
+#n1=int(input("enter any number "))
+#n2=int(input("enter any number "))
+#n3=int(input("enter any number "))
+#n4=int(input("enter any number "))
+#numbers = {n1,n2,n3,n4}
+#print(numbers)
+#if(n1>n2 and n1>n3 and n1>n4):   
+#  print("n1 is largent of all")
+#elif  (n2>n1 and n2>n3 and n2>n4):
+#  print("n2 is largent of all")
+#elif  (n3>n1 and n3>n4 and n3>n2):
+#  print("n3 is largent of all")
+#else:
+#  print("n4 is the largest")
+  
+#n1=int(input("enter marks  "))
+#n2=int(input("enter marks  "))
+#n3=int(input("enter marks  "))
+#subjects={n1,n2,n3}
+#marks = (((n1+n2+n3)/3))
+#print(marks )
+#if((n1>=33 and n2>=33 and n3>=33)  and marks>=40):
+ #  print("you passed in the subject and total")
+#elif(marks>=40):
+#    print("you are pass")
+#else :
+
+ # print("you failed in both try hard next time ")
+
+# problem 03
+#p1 = "Mske a lot of money"
+#p2 = " buy now "
+##p3 = " subscribe this "
+##p4 = " click this "
+#
+#
+
+#message = input("enter text: ")
+#if (p1 in message or p2 in message or p3 in message or p4 in message):
+##    print("this is spam ")
+##else :
+##    print("this is not spam")    
+
+# problem no. 4
+##p=input("enter your name ")
+##list = [
+    #"Rahul Sharma",
+    #"Ananya Verma",
+    #"David Johnson",
+   # "Priya Kapoor",
+    #"Michael Lee",
+    #"Sneha Reddy",
+    #"Arjun Mehta",
+    #"Emma Wilson",
+    #"Rohan Singh",
+    #"Aisha Khan",
+    #"Daniel Martin",
+    #"Meera Nair"
+#]
+##if ( p in list):##
+    #print ("kundi mat khatkau raja sidha andar aayo raja ")
+##else :##
+   # print( "chala ja bhunsdi wale ")
+
+# problem no.5
+#marks = int(input("enter your marks: "))
+#if 90 <= marks <= 100:
+#         print("Ex")
+#elif 80 <= marks < 90:
+#        print ("A")
+#elif 70 <= marks < 80:
+#        print ("B")
+#elif 60 <= marks < 70:
+#        print ("C")
+#elif 50 <= marks < 60:
+#        print ("D")
+#else:##
+#        print("f")
+
+# problem no 74
+#qualities = {
+    #"q0": "muskan",
+    #"q3": "brahmin",
+    #"q6": "sant",
+    #"q4": "punjabi",
+    #"q9": "mmdu",
+   # "q7": "owner",
+   # "q5": "tall",
+   # "q10": "tech"
+#}
+#message =input( "message")
+#if (value in message for value in qualities.values()):
+ #   print("he is gks")
+#else :
+  #  print("jata kyu nhi maderchod")    
+
+user = input("enter username: ")
+if (len(user<10)):
+    print("ok ")
+else:
+    print("not correct")     
